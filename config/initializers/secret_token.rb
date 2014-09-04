d122286b56b2d397a061305dcb5a7a924199434b
max <mk@kernel-corp.com>
2014-09-04 14:57:28 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Converzilla::Application.config.secret_key_base = '2d57b6f05de309fa1a883ef6e828646bce4f9a217bf19da9d6cf17bc544a7ea0c8a9f58b6ee4e959bedcea060d4e1b171dfe49207328dd9a9aa92629360b7e21'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Converzilla::Application.config.session_store :cookie_store, key: '_converzilla_session'
