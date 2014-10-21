29d645d227ceb9475fd0a248c9b6187ad3e18c44
max <mk@kernel-corp.com>
2014-10-21 15:26:41 +0700

    hotfix 2

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,7 +1,7 @@
-class VisitorsController < ApplicationController
+class VisitsController < ApplicationController
   def create
     client  = Client.find params[:client_id]
-    visitor = client.visitors.find params[:visitor]
+    visitor = client.visitors.find params[:visitor_id]
     visitor.visits << Visit.new
     head :ok
   end
